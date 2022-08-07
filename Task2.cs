70950aec171a8ad6a1a8ccc21c4b11f315eccdbf
Sarge <sarge2033@gmail.com>
2022-08-07 15:13:19 +0300

    первое задание

---
+++ Task2.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 2 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b)
+{   
+    Console.WriteLine("Они равны");
+}    
+else if(a > b)
+{   
+    Console.WriteLine("Большее " +a); 
+    Console.WriteLine("Меньшее " +b);
+}
+else
+{
+    Console.WriteLine("Большее " +b);
+    Console.WriteLine("Меньшее " +a);
+}
